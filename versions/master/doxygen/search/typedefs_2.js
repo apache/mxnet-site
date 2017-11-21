17379812e0aaad59140d584dbaeab6b2a1192113
jenkins <users@infra.apache.org>
2017-11-21 08:22:42 +0000

    Nightly build

--- versions/master/doxygen/search/typedefs_2.js
+++ versions/master/doxygen/search/typedefs_2.js
@@ -9,13 +9,11 @@ var searchData=
   ['cudamodulehandle',['CudaModuleHandle',['../c__api_8h.html#aeef73e8188f1cf7f79a13517f5828693',1,'c_api.h']]],
   ['customfunctionbwdfunc',['CustomFunctionBwdFunc',['../c__api_8h.html#ad71379747c8a269676425aa012323227',1,'c_api.h']]],
   ['customfunctiondelfunc',['CustomFunctionDelFunc',['../c__api_8h.html#a5de746fbeeb2f6ca3cf7745ad393d7cb',1,'c_api.h']]],
-  ['customopbackwardinferstoragetypefunc',['CustomOpBackwardInferStorageTypeFunc',['../c__api_8h.html#a9d418386b1b9c5e3d6a6823788dc6146',1,'c_api.h']]],
   ['customopbwddepfunc',['CustomOpBwdDepFunc',['../c__api_8h.html#ae8feb033b715209653ea91e089d060b0',1,'c_api.h']]],
   ['customopcreatefunc',['CustomOpCreateFunc',['../c__api_8h.html#a201e319981a60877ab9d6f7cf3b3f62d',1,'c_api.h']]],
   ['customopdelfunc',['CustomOpDelFunc',['../c__api_8h.html#a557fa6ce86e94109ce7797b2cce33e23',1,'c_api.h']]],
   ['customopfbfunc',['CustomOpFBFunc',['../c__api_8h.html#ac153c43c858c3070c63d0eec44f36514',1,'c_api.h']]],
   ['customopinfershapefunc',['CustomOpInferShapeFunc',['../c__api_8h.html#a9a559987f509f9939030addd565d34a3',1,'c_api.h']]],
-  ['customopinferstoragetypefunc',['CustomOpInferStorageTypeFunc',['../c__api_8h.html#ad38e271bde5b85a9ba4dea6c7a1cddbe',1,'c_api.h']]],
   ['customopinfertypefunc',['CustomOpInferTypeFunc',['../c__api_8h.html#a09da22ebe9a09b084afc386fa7b0ef53',1,'c_api.h']]],
   ['customoplistfunc',['CustomOpListFunc',['../c__api_8h.html#add8141d7f51e3a77544cc7b79433d16e',1,'c_api.h']]],
   ['customoppropcreator',['CustomOpPropCreator',['../c__api_8h.html#a3a7fd875ee6aad3f20981764626a6c7b',1,'c_api.h']]]
