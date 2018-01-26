d37ad427aa38325dd08ee34b984a5886b8044f48
mxnet-ci <mxnet-ci>
2018-01-26 01:44:21 +0000

    Nightly build

--- versions/master/doxygen/html/search/variables_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['callbacks',['callbacks',['../structMXCallbackList.html#ad56325e57f5fffc2b920eb3f8f378199',1,'MXCallbackList']]],
-  ['contexts',['contexts',['../structMXCallbackList.html#a332323a5f414bd3f5e423788804a0dcd',1,'MXCallbackList']]],
-  ['ctx',['ctx',['../structmxnet_1_1RunContext.html#ace5970ed7c25ccc98ffd0dcf35fd3c88',1,'mxnet::RunContext::ctx()'],['../classmxnet_1_1Imperative_1_1AGInfo.html#afe2cb4d45bd32e40a86d2a49bfd20952',1,'mxnet::Imperative::AGInfo::ctx()'],['../structmxnet_1_1Storage_1_1Handle.html#ae0a4202582aa12a598f5c55ef51ad4c8',1,'mxnet::Storage::Handle::ctx()']]]
-];
